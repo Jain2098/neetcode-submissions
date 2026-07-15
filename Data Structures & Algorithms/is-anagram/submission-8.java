29e4ec361782f1766b5c1d987f2f58b4aa47dc3e
Himanshu <41346368+Jain2098@users.noreply.github.com>
2026-07-15 19:35:29 -0400

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length()!=t.length()){
+            return false;
+        }
+        
+        char[] a1 = s.toCharArray();
+        char[] a2 = t.toCharArray();
+
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+
+        return Arrays.compare(a1, a2)==0;
+
+    }
+}
