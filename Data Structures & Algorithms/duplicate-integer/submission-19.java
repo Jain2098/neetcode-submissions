6612ff142e09c2d074107e36083d36cc38ff03cc
Himanshu <41346368+Jain2098@users.noreply.github.com>
2026-07-18 19:23:51 -0400

    Add: duplicate-integer - submission-19

---
+++ Data Structures & Algorithms/duplicate-integer/submission-19.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+        Set<Integer> seen = new HashSet<>();
+
+        for (int i: nums){
+            if (!seen.add(i)){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
